f442cb96a8d6907415d5fefe173cf10b297f18c9
Jorgeerodriguez <tri.jorge_rodriguez@yahoo.com>
2025-02-20 20:45:58 -0600

    Changes From Jorge

--- whsxc/views.py
+++ whsxc/views.py
@@ -9,6 +9,7 @@ def homepage(request):
             #datelist:
         },)
 
+
 def index(request):
     return render(request, "blog/entry_archive.html",  {
             "latest": Entry.objects.all().order_by("-created_at")[:5],
