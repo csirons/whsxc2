c62d083ecfe6a089701e9315bef507dedc780bee
Jorgeerodriguez <tri.jorge_rodriguez@yahoo.com>
2025-03-15 18:25:26 -0500

    chore: Add cross country extras template tags for runners.html template and URL Runners is workign fine but no display of runners.

--- runners/models.py
+++ runners/models.py
@@ -1,36 +1,21 @@
-
-
 from django.db import models
+from django.contrib.admin import TabularInline
+from django.dispatch import dispatcher
+from django.db.models import signals
+#from crosscountry import signals as mysignals
 
+#import urllib2 # thi s is for version 2.x
+import urllib # this is for version 3.x
 import datetime
 import re
 
+#import route_url_lib
 
-
-
+route_urls = {
+  #http://runkeeper.com/user/315746892/activity/56941773
+  r'https?:\/\/runkeeper.com\/user\/[0-9]+\/activity\/[0-9]+': lambda url: route_url_lib.runkeeper(url),
+}
 # Create your models here.
-
-GENDER_CHOICES = (
-  ('M', 'Male'),
-  ('F', 'Female'),
-)
-
-class MaleManager(models.Manager):
-  def get_query_set(self):
-    return super(MaleManager, self).get_query_set().filter(gender='M')
-
-class FemaleManager(models.Manager):
-  def get_query_set(self):
-    return super(FemaleManager, self).get_query_set().filter(gender='F')
-  
-class Course(models.Model):
-  name = models.CharField(max_length=255)
-  description = models.TextField(null=True,blank=True)
-  directions = models.TextField(null=True,blank=True)
-  address1 = models.CharField(max_length=255,null=True,blank=True)
-  zip_code = models.PositiveIntegerField(null=True,blank=True)
-  route_url = models.CharField('Route URL', max_length=255,null=True,blank=True)
-  route = models.TextField(null=True,blank=True)
 
 class RunnerManager(models.Manager):
   def by_year(self,year):
@@ -52,7 +37,18 @@ class RunnerManager(models.Manager):
       result_list.append(p)
     return result_list
 
+class MaleManager(models.Manager):
+  def get_query_set(self):
+    return super(MaleManager, self).get_query_set().filter(gender='M')
 
+class FemaleManager(models.Manager):
+  def get_query_set(self):
+    return super(FemaleManager, self).get_query_set().filter(gender='F')
+
+GENDER_CHOICES = (
+  ('M', 'Male'),
+  ('F', 'Female'),
+)
 
 class Runner(models.Model):
   name = models.CharField(max_length=255)
@@ -110,6 +106,35 @@ class Runner(models.Model):
     average = average / len(season_runs)
     return average
 
+class Course(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField(null=True,blank=True)
+  directions = models.TextField(null=True,blank=True)
+  address1 = models.CharField(max_length=255,null=True,blank=True)
+  zip_code = models.PositiveIntegerField(null=True,blank=True)
+  route_url = models.CharField('Route URL', max_length=255,null=True,blank=True)
+  route = models.TextField(null=True,blank=True)
+
+  class Meta:
+    ordering = ['name']
+
+  class Admin:
+    list_display = ('name','address1','zip_code')
+
+  def __unicode__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return	"/courses/%i/" % self.id
+
+  def save(self):
+    if not self.route and self.route_url:
+      for r in route_urls:
+        p = re.compile(r)
+        if p.match(self.route_url):
+          self.route = route_urls[r](self.route_url)
+
+    super(Course, self).save()
 
 class MeetManager(models.Manager):
   def years(self):
@@ -125,7 +150,7 @@ class MeetManager(models.Manager):
 
 class Meet(models.Model):
   name = models.CharField(max_length=255, blank=True)
-  #course = models.ForeignKey(Course)
+  course = models.ForeignKey(Course,on_delete=models.SET_NULL, null=True, blank=True) # course can be null for meets that are not on a specific course on_delete=models.CASCADE,
   occurred_at = models.DateTimeField('date')
   num_teams = models.IntegerField(null=True,blank=True)
   official_results_url = models.URLField(blank=True, null=True)
@@ -193,3 +218,254 @@ class Meet(models.Model):
       url+'sort-place/',
       url+'sort-points/',
     ]
+
+TEAM_CHOICES = (
+    ('1GV', 'Girls Varsity'),
+    ('2BV', 'Boys Varsity'),
+    ('3GJ', 'Girls JV'),
+    ('4BJ', 'Boys JV'),
+    ('5GF', 'Girls Frosh/Soph'),
+    ('6BF', 'Boys Frosh/Soph'),
+)
+TEAM_CHOICES_REVERSE = {
+    '1GV': 'Girls Varsity',
+    '2BV': 'Boys Varsity',
+    '3GJ': 'Girls JV',
+    '4BJ': 'Boys JV',
+    '5GF': 'Girls Frosh/Soph',
+    '6BF': 'Boys Frosh/Soph',
+}
+
+class Race(models.Model):
+  meet = models.ForeignKey(Meet,on_delete=models.SET_NULL, null=True, blank=True)
+  team = models.CharField(max_length=3, choices=TEAM_CHOICES)
+  time = models.TimeField(null=True,blank=True)
+  num_runners = models.IntegerField(null=True,blank=True)
+  team_place = models.IntegerField(null=True,blank=True)
+  num_teams = models.IntegerField(null=True,blank=True)
+
+  #updated automagically
+  top_finish = models.IntegerField(null=True,editable=False)
+  pack_time = models.IntegerField(null=True,editable=False)
+
+  #denormalizing info
+  meet_name = models.CharField(max_length=255, editable=False)
+  course_name = models.CharField(max_length=255,editable=False)
+  occurred_at = models.DateTimeField('date',editable=False)
+
+  def save(self):
+    self.course_name = self.meet.course.name
+    self.occurred_at = self.meet.occurred_at
+    self.meet_name = "%s" % self.meet
+
+    super(Race,self).save()
+
+  class Admin:
+    list_display = ('meet','team','course_name','team_place','top_finish','pack_time')
+    list_filter = ['team','occurred_at','course_name']
+    date_hierarchy = 'occurred_at'
+    js = [ '/media/javascript/convert_times.js' ]
+
+  def __unicode__(self):
+    return "%s - %s" % (self.meet_name, TEAM_CHOICES_REVERSE[self.team])
+
+  def sorted_runs(self):
+    return self.run_set.order_by('final_time')
+
+  def update_denormalized(self, save=False):
+    pass
+
+  def calc_race_info(self, save=False):
+    runs = self.run_set.order_by('final_time')
+
+    self.top_finish = None
+    for run in runs:
+      if run.place:
+        self.top_finish = run.place
+        break
+
+    if len(runs) < 2:
+      self.pack_time = None
+    else:
+      self.pack_time = runs[len(runs)-1].final_time - runs[0].final_time
+
+    if save:
+      self.save()
+
+class FreshmanRunsManager(models.Manager):
+  def get_query_set(self):
+    return super(FreshmanRunsManager, self).get_query_set().filter(grade=4).order_by('final_time')
+
+class SophomoreRunsManager(models.Manager):
+  def get_query_set(self):
+    return super(SophomoreRunsManager, self).get_query_set().extra(tables=['crosscountry_runner'],where=['crosscountry_runner.id = crosscountry_run.runner_id',("(crosscountry_runner.year - strftime('%%Y',crosscountry_run.occurred_at)) = 3")]).order_by('final_time')
+
+class JuniorRunsManager(models.Manager):
+  def get_query_set(self):
+    return super(JuniorRunsManager, self).get_query_set().extra(tables=['crosscountry_runner'],where=['crosscountry_runner.id = crosscountry_run.runner_id',("(crosscountry_runner.year - strftime('%%Y',crosscountry_run.occurred_at)) = 2")]).order_by('final_time')
+
+class SeniorRunsManager(models.Manager):
+  def get_query_set(self):
+    return super(SeniorRunsManager, self).get_query_set().extra(tables=['crosscountry_runner'],where=['crosscountry_runner.id = crosscountry_run.runner_id',("(crosscountry_runner.year - strftime('%%Y',crosscountry_run.occurred_at)) = 1")]).order_by('final_time')
+
+class Run(models.Model):
+  #runner = models.ForeignKey(Runner, edit_inline=models.TABULAR)
+  #race = models.ForeignKey(Race, edit_inline=models.TABULAR, min_num_in_admin=7)
+  runner = models.ForeignKey(Runner, on_delete=models.CASCADE)
+  race = models.ForeignKey(Race, on_delete=models.CASCADE)
+  mile_1_time = models.IntegerField(null=True,blank=True)
+  mile_2_time = models.IntegerField(null=True,blank=True)
+  split_2 = models.IntegerField(null=True,editable=False)
+  split_3 = models.IntegerField(null=True,editable=False)
+  #final_time = models.IntegerField(core=True)
+  final_time = models.IntegerField(null=True,blank=True) # in seconds
+  place = models.IntegerField(null=True,blank=True)
+
+  # calculate automagically
+  letter_points = models.IntegerField(null=True,editable=False)
+  pr = models.BooleanField(editable=False)
+  pr_course = models.BooleanField(editable=False)
+  sb = models.BooleanField(editable=False)
+  all_time_rank = models.IntegerField(editable=False)
+  grade_rank = models.IntegerField(editable=False)
+  course_rank = models.IntegerField(editable=False)
+
+  # denormalizing info
+  gender = models.CharField(max_length=1,editable=False)
+  meet = models.ForeignKey(Meet,editable=False, on_delete=models.CASCADE)
+  course = models.ForeignKey(Course, editable=False, on_delete=models.CASCADE)
+  occurred_at = models.DateTimeField('date', editable=False)
+  grade = models.IntegerField(editable=False)
+
+  # managers
+  objects = models.Manager()
+  freshmanruns = FreshmanRunsManager()
+  sophomoreruns = SophomoreRunsManager()
+  juniorruns = JuniorRunsManager()
+  seniorruns = SeniorRunsManager()
+
+  class Admin:
+    pass
+
+  def __unicode__(self):
+    return "Run for %s" % self.runner.name
+
+  def calc_letter_points(self):
+    points = 0
+
+    if self.race.team.find('V') >= 0:
+      points = points + 15
+
+      if self.place and self.place <= 15:
+        points = points + 50
+
+      if self.place and self.place == 1:
+          points = points + 50
+
+      if self.place and self.race.num_runners:
+        place_percent = (1.*self.place)/self.race.num_runners
+        if self.place == 1:
+          points = points + 0
+        elif place_percent < 0.1:
+          points = points + 20
+        elif place_percent < 0.2:
+          points = points + 15
+        elif place_percent < 0.3:
+          points = points + 10
+        elif place_percent < 0.4:
+          points = points + 5
+        elif place_percent < 0.5:
+          points = points + 3
+
+      meet_place = None
+      if self.gender == 'M':
+        meet_place = self.meet.team_place_male
+      else:
+        meet_place = self.meet.team_place_female
+
+      if meet_place:
+        if meet_place == 1:
+          points = points + 50
+        elif meet_place == 2:
+          points = points + 20
+        elif meet_place == 3:
+          points = points + 10
+        elif meet_place == 4:
+          points = points + 5
+
+    elif self.race.team.find('J') >= 0:
+      points = points + 5
+      if self.place and self.race.num_runners:
+        place_percent = (1.*self.place)/self.race.num_runners
+        if self.place == 1:
+          points = points + 20
+        elif place_percent < 0.1:
+          points = points + 15
+        elif place_percent < 0.2:
+          points = points + 10
+        elif place_percent < 0.3:
+          points = points + 5
+        elif place_percent < 0.4:
+          points = points + 3
+
+    if self.pr_course:
+      points = points + 10
+    elif self.pr:
+      points = points + 5
+
+    self.letter_points = points
+
+  def update_denormalized(self, save=False):
+    pass
+
+  def calc_rankings(self):
+    self.all_time_rank = Run.objects.filter(gender=self.gender).filter(final_time__lt=self.final_time).count()+1
+    self.course_rank = Run.objects.filter(gender=self.gender).filter(course=self.course).filter(final_time__lt=self.final_time).count()+1
+    self.grade_rank = Run.objects.filter(gender=self.gender).filter(grade=self.grade).filter(final_time__lt=self.final_time).count()+1
+
+  def save(self):
+    self.gender = self.runner.gender
+    self.meet = self.race.meet
+    self.course = self.meet.course
+    self.occurred_at = self.meet.occurred_at
+    self.grade = self.runner.year - self.occurred_at.year
+
+    self.calc_rankings()
+
+    if self.mile_1_time and self.mile_2_time:
+      self.split_2 = self.mile_2_time - self.mile_1_time
+    if self.mile_2_time:
+      self.split_3 = self.final_time - self.mile_2_time
+
+    self.calc_letter_points()
+
+    super(Run,self).save()
+
+  def calc_personal_records(self, save=False):
+    pr_course = self.runner.pr_course(self.course_id, self.occurred_at)
+    if pr_course and self.final_time < pr_course[0].final_time:
+      self.pr_course = True
+    else:
+      self.pr_course = False
+
+    pr = self.runner.pr(self.occurred_at)
+    if pr and self.final_time < pr[0].final_time:
+      self.pr = True
+    else:
+      self.pr = False
+
+    sb = self.runner.sb(self.occurred_at)
+    if sb and self.final_time < sb[0].final_time:
+      self.sb = True
+    else:
+      self.sb = False
+
+    if save:
+      self.save()
+
+  def mile_3_pace(self):
+    return (self.split_3)*(1./1782)*1609
+
+  def year(self):
+    return self.occurred_at.year
+
---
+++ runners/templatetags/crosscountry_extras.py
@@ -0,0 +1,212 @@
+from django import template
+from django.template import Variable
+
+register = template.Library()
+
+def gender_name(value):
+  return {
+    'F': 'Girls',
+    'M': 'Boys',
+  }[value]
+register.filter('gender_name', gender_name)
+
+def less_than(value, num):
+  if not value:
+    return False
+  return value < int(num)
+register.filter('less_than', less_than)
+
+def have_team_place(meets):
+  for meet in meets:
+    if meet.team_place_male or meet.team_place_female:
+      return True
+  return False
+register.filter('have_team_place', have_team_place)
+
+def have_top_finish(meets):
+  for meet in meets:
+    if meet.top_finish_male or meet.top_finish_female:
+      return True
+  return False
+register.filter('have_top_finish', have_top_finish)
+
+def have_pack_time(meets):
+  for meet in meets:
+    if meet.pack_time_male or meet.pack_time_female:
+      return True
+  return False
+register.filter('have_pack_time', have_pack_time)
+
+def time_tba(time):
+  if time.hour == 0 and time.minute == 0:
+    return 'TBA'
+  else:
+    hour = time.hour
+    ampm = 'am'
+    if hour > 12:
+      hour = hour - 12
+      ampm = 'pm'
+    return str(hour) + ':' + time.strftime('%M') + ampm
+register.filter('time_tba', time_tba)
+
+def grade_name(value):
+  return {
+    '1': 'Senior',
+    '2': 'Junior',
+    '3': 'Sophomore',
+    '4': 'Freshman',
+  }[str(value)]
+register.filter('grade_name', grade_name)
+
+def grade_name_plural(value):
+  return {
+    '1': 'Seniors',
+    '2': 'Juniors',
+    '3': 'Sophomores',
+    '4': 'Freshmen',
+  }[str(value)]
+register.filter('grade_name_plural', grade_name_plural)
+
+def team_name(value):
+  return {
+    '1GV': 'Girls Varsity',
+    '2BV': 'Boys Varsity',
+    '3GJ': 'Girls JV',
+    '4BJ': 'Boys JV',
+    '5GF': 'Girls Frosh/Soph',
+    '6BF': 'Boys Frosh/Soph',
+  }[value]
+register.filter('team_name', team_name)
+
+def convert_seconds(value):
+  if str(value.__class__) == "<type 'unicode'>":
+    return value
+  else:
+    return "%d:%02d" % (value/60,value%60)
+register.filter('convert_seconds',convert_seconds)
+
+def ifnotnull(value):
+  if value:
+    return value
+  else:
+    return ""
+register.filter('ifnotnull',ifnotnull)
+
+def list_runs(females, males, hide_date=None):
+  return { 'female_runs': females, 'male_runs': males, 'hide_date': hide_date }
+register.inclusion_tag('crosscountry/runs_list.html')(list_runs)
+
+def runs_list(females, males, title):
+  return { 'female_runs': females, 'male_runs': males, 'title': title }
+register.inclusion_tag('crosscountry/_runs_list.html')(runs_list)
+
+def meets_list(meets, show_year=None):
+  return { 'meets': meets, 'show_year': show_year }
+register.inclusion_tag('crosscountry/_meets_list.html')(meets_list)
+
+def load_runs(parser,token):
+   return LoadRunsNode()
+
+class LoadRunsNode(template.Node):
+  def render(self, context):
+    context['runs'] = context['race'].run_set.order_by(context['sort_runs_by'])
+    return ''
+register.tag('load_runs',load_runs)
+
+def sectionbyattribute(parser,token):
+  try:
+    tag_name, attribute, objects_list = token.split_contents()
+  except ValueError:
+    raise template.TemplateSyntaxError("%r tag requires two arguments" % token.contents[0])
+
+  node_list = parser.parse(('endsectionbyattribute',))
+  parser.delete_first_token()
+  return SectionByAttribute(attribute, objects_list, node_list)
+
+class SectionByAttribute(template.Node):
+  def __init__(self, attribute, objects_list, node_list):
+    if attribute[0] == attribute[-1] and attribute[0] in ('"', "'"):
+      self.attribute_name = self.attribute = attribute[1:-1]
+      self.attribute_variable = None
+    else:
+      self.attribute = None
+      self.attribute_name = attribute
+      self.attribute_variable = template.Variable(attribute)
+    self.objects_list = Variable(objects_list)
+    self.objects_list_name = objects_list.replace('.','_')
+    self.node_list = node_list
+
+  def render(self, context):
+    sections = []
+    current_section = 0
+    
+    if self.attribute_variable:
+      self.attribute = self.attribute_variable.resolve(context)
+    
+    for object in self.objects_list.resolve(context):
+      attr = getattr(object,self.attribute)
+      
+      if callable(attr):
+        attr = attr()
+      
+      if attr != current_section:
+        current_section = attr
+        sections.append( { self.attribute: current_section, self.objects_list_name: [] } )
+      sections[-1][self.objects_list_name].append(object)
+
+    output = ""
+    for section in sections:
+      context[self.attribute_name] = section
+      output += self.node_list.render(context)
+      del context[self.attribute_name]
+
+    return output
+register.tag('sectionbyattribute',sectionbyattribute)
+
+def sum(parser,token):
+  try:
+    tag_name, list, field_name = token.split_contents()
+  except ValueError:
+    raise template.TemplateSyntaxError("%r tag requires two arguments" % token.contents[0])
+
+  return SumNode(list,field_name)
+
+class SumNode(template.Node):
+  def __init__(self, list, field_name):
+    self.list = Variable(list)
+    self.field_name = field_name
+
+  def render(self, context):
+    sum = 0
+    for item in self.list.resolve(context):
+      sum += getattr(item,self.field_name)
+    return sum
+register.tag('sum',sum)
+
+def get_dict_value(parser,token):
+  try:
+    tag_name, key, fromWord, dict, asWord, name = token.split_contents()
+  except ValueError:
+    raise template.TemplateSyntaxError("%r tag requires five arguments" % token.contents[0])
+  return GetDictValue(key, dict, name)
+
+class GetDictValue(template.Node):
+  def __init__(self, key, dict, name):
+    if key[0] == key[-1] and key[0] in ('"', "'"):
+      self.key_str = self.key = key[1:-1]
+      self.key_variable = False
+    else:
+      self.key_str = False
+      self.key_variable = template.Variable(key)
+    self.dict = Variable(dict)
+    self.name = name
+
+  def render(self, context):
+    if self.key_str:
+      context[self.name] = self.dict.resolve(context)[self.key_str]
+    else:
+      context[self.name] = self.dict.resolve(context)[self.key_variable.resolve(context)]
+
+    return ''
+register.tag('get_dict_value',get_dict_value)
+
