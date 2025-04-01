ca8dca42040d0afae41145384c69598d947d4a7f
csirons <csirons@yahoo.com>
2025-04-01 17:36:43 -0600

    History/schedule

--- whsxc/views.py
+++ whsxc/views.py
@@ -74,16 +74,135 @@ def meet_detail(request, object_id=-1, sort=None):
     return render(request, 'meets/meet_detail.html', { 'meet': meet, 'races': races, 'sort_runs_by': sort_field,},)
 
 def history(request):
-    return render(request, 'history/history.html', {
-            "latest": Entry.objects.all().order_by("-created_at")[:5],
-            #datelist:
+    best_runners_male = Run.objects.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')[:10]
+    #best_runners_male = Run.objects.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')#.group_by('runner_id')[:10]
+    best_runners_female = Run.objects.extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).filter(gender='F').order_by('best_time')[:10]
+
+    freshman_runners_male = Run.freshmanruns.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')[:10]
+    freshman_runners_female = Run.freshmanruns.extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).filter(gender='F').order_by('best_time')[:10]
+    sophomore_runners_male = Run.sophomoreruns.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')[:10]
+    sophomore_runners_female = Run.sophomoreruns.extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).filter(gender='F').order_by('best_time')[:10]
+    junior_runners_male = Run.juniorruns.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')[:10]
+    junior_runners_female = Run.juniorruns.extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).filter(gender='F').order_by('best_time')[:10]
+    senior_runners_male = Run.seniorruns.filter(gender='M').extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).order_by('best_time')[:10]
+    senior_runners_female = Run.seniorruns.extra(select={'best_time': 'MIN("crosscountry_run"."final_time")'}).filter(gender='F').order_by('best_time')[:10]
+
+    best_runs_male = Run.objects.select_related().filter(gender='M').order_by('final_time')[:10]
+    best_runs_female = Run.objects.select_related().filter(gender='F').order_by('final_time')[:10]
+
+    freshman_runs_male = Run.freshmanruns.filter(gender='M').order_by('final_time')[:10]
+    freshman_runs_female = Run.freshmanruns.filter(gender='F').order_by('final_time')[:10]
+
+    sophomore_runs_male = Run.sophomoreruns.filter(gender='M').order_by('final_time')[:10]
+    sophomore_runs_female = Run.sophomoreruns.filter(gender='F').order_by('final_time')[:10]
+
+    junior_runs_male = Run.juniorruns.filter(gender='M').order_by('final_time')[:10]
+    junior_runs_female = Run.juniorruns.filter(gender='F').order_by('final_time')[:10]
+
+    senior_runs_male = Run.seniorruns.filter(gender='M').order_by('final_time')[:10]
+    senior_runs_female = Run.seniorruns.filter(gender='F').order_by('final_time')[:10]
+
+    #return render(request, 'summerrunning/summerrunning.html', {
+    #        "latest": Entry.objects.all().order_by("-created_at")[:5],
+    #        #datelist:
+    #    },)
+
+    return render(request,'runners/top10_list.html', {
+        'best_runners_male': best_runners_male,
+        'best_runners_female': best_runners_female,
+        'freshman_runners_male': freshman_runners_male,
+        'freshman_runners_female': freshman_runners_female,
+        'sophomore_runners_male': sophomore_runners_male,
+        'sophomore_runners_female': sophomore_runners_female,
+        'junior_runners_male': junior_runners_male,
+        'junior_runners_female': junior_runners_female,
+        'senior_runners_male': senior_runners_male,
+        'senior_runners_female': senior_runners_female,
+        'best_runs_male': best_runs_male,
+        'best_runs_female': best_runs_female,
+        'freshman_runs_male': freshman_runs_male,
+        'freshman_runs_female': freshman_runs_female,
+        'sophomore_runs_male': sophomore_runs_male,
+        'sophomore_runs_female': sophomore_runs_female,
+        'junior_runs_male': junior_runs_male,
+        'junior_runs_female': junior_runs_female,
+        'senior_runs_male': senior_runs_male,
+        'senior_runs_female': senior_runs_female,
         },)
 
+def runner_detail(request, object_id=-1, sort=None, organize="year"):
+  # calculate which years has been running
+  # calculate letter points for those years using a group by and SUM
+
+  runner = get_object_or_404(Runner, pk=object_id)
+
+  if sort == 'date':
+    return HttpResponsePermanentRedirect(runner.get_absolute_url())
+
+  if sort == None:
+    sort = 'date'
+
+  sort_field = {
+    'split1': 'mile_1_time',
+    'split2': 'split_2',
+    'split3': 'split_3',
+    'final': 'final_time',
+    'place': 'place',
+    'points': 'letter_points',
+    'date': '-occurred_at',
+  }[sort]
+
+  if not organize:
+    organize = 'year'
+
+  organize_field = {
+    'year': 'year',
+    'course': 'course',
+  }[organize]
+
+  organize_direction = {
+    'year': '-',
+    'course': '',
+  }[organize]
+
+  runs = runner.run_set.extra(select={'year': 'strftime("%%Y",occurred_at)'}).order_by(organize_direction+organize_field,sort_field)
+ 
+  #return render(request, 'summerrunning/summerrunning.html', {
+  #          "latest": Entry.objects.all().order_by("-created_at")[:5],
+  #          #datelist:
+  #      },)
+
+
+  return render(request,'runners/runner_detail.html', { 'runner':runner, 'runs':runs, 'sort_runs_by':sort_field, 'organize_field':organize_field },)
+
+
+    
+
+
+    
 def schedule(request):
-    return render(request, 'schedule/schedule.html', {
-            "latest": Entry.objects.all().order_by("-created_at")[:5],
-            #datelist:
-        },)
+  now = datetime.datetime.now()
+  #now = datetime.datetime.now() + datetime.timedelta(days=200)
+  meets = Meet.objects.select_related().filter(occurred_at__year=now.year).order_by('occurred_at')
+
+  next_meet = None
+  for meet in meets:
+    if not meet.past_tense():
+      meet.next_meet = True
+      next_meet = meet
+      break
+  for meet in meets:
+    meet.races = meet.race_set.order_by('time')
+    if len(meet.races) > 0:
+      meet.have_race_times = True
+      for race in meet.races:
+        if not race.time:
+          meet.have_race_times = False
+    else:
+      meet.have_race_times = False
+
+
+  return render(request, 'schedule/schedule.html', { 'meets': meets, 'now': now, 'next_meet': next_meet, },)
 
 def homemeet(request):
     return render(request, 'homemeet/homemeet.html', {
