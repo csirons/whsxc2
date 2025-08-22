372761111646d84ef5029b1a8f2997f65b941c70
Benjamin Thomas <ben@bentomas.com>
2025-08-22 10:20:42 -0700

    when saving a race or runner, updated connected data/rows

--- crosscountry/admin.py
+++ crosscountry/admin.py
@@ -13,6 +13,21 @@ class RaceAdmin(admin.ModelAdmin):
     js = [ '/media/javascript/convert_times.js' ]
     inlines = [RunInline]
 
+    def save_model(self, request, race, form, change):
+         super().save_model(request, race, form, change)
+
+         # race = get_object_or_404(Race, pk=race_id)
+         race.update_denormalized()
+         race.calc_race_info()
+         race.save()
+         race.meet.calc_races_info(save=True)
+
+         for run in race.run_set.all():
+           run.update_denormalized()
+           run.calc_personal_records()
+           run.calc_letter_points()
+           run.save()
+
 
 @admin.register(Runner)
 class RunnerAdmin(admin.ModelAdmin):
@@ -20,6 +35,23 @@ class RunnerAdmin(admin.ModelAdmin):
     #list_filter = ['team','occurred_at','course_name']
     list_filter = ['name', 'year', 'gender']
     js = [ '/media/javascript/convert_times.js' ]
+
+    def save_model(self, request, race, form, change):
+         super().save_model(request, race, form, change)
+
+         for run in runner.run_set.order_by('id'):
+           race = run.race
+           race.update_denormalized()
+           race.calc_race_info()
+           race.save()
+
+           run.meet.calc_races_info(save=True)
+
+           run.update_denormalized()
+           run.calc_personal_records()
+           run.calc_letter_points()
+           run.save()
+
 
 
 @admin.register(Course)
